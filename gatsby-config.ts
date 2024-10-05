1a3cf12f0f275931f6bf287387a9629a1b495b48
Mat Sz <contact@matsz.dev>
2024-10-05 14:42:39 +0200

    chore: add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,20 +3,38 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Metastable`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://metastable.studio`,
   },
+
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Metastable`,
+        short_name: `Metastable`,
+        start_url: `/`,
+        background_color: `#11111a`,
+        theme_color: `#77f`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+  ],
 };
 
 export default config;
